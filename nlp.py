ce81cbaec4b89e8bcd3b58ba9298f6d67a3b360a
Rupsha <ritwikray2005@gmail.com>
2026-08-06 03:04:45 +0530

    display bug

--- nlp.py
+++ nlp.py
@@ -171,7 +171,7 @@ def label_stories(stories:list[dict[str,str]], entity_type:Literal['EST', 'OPP']
                 'sentence': datapoint,
                 'tone': sent_tone,
                 'confidence': round(max(vector) * 100, ndigits=2),
-                'vector': vector
+                'vector': [round((elem) * 100, ndigits=2) for elem in vector]
             })
         
         if not vectors:
